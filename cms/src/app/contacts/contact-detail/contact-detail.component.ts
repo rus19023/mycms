730c53bc53893825c30feba5dccd20fe25d527ba
Doris Rush-Lopez <rus19023@byui.edu>
2022-09-25 00:51:02 -0400

    contact detail done,missing content, style errors

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact(1, 'R. Kent Jackson','jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', []),
+  new Contact(2, 'Rex Barzee','barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', [])
+  ];
 
   constructor() { }
 
--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,14 +1,14 @@
 export class Contact {
     public id: number;
-    public name: string;
+    public cname: string;
     public email: string;
     public phone: string;
     public imageUrl: string;
     public group: number[];
 
-    constructor(id: number, name: string, email: string, phone: string, imageUrl: string, group: number[]) {
+    constructor(id: number, cname: string, email: string, phone: string, imageUrl: string, group: number[]) {
         this.id = id;
-        this.name = name;
+        this.cname = cname;
         this.email = email;
         this.phone = phone;
         this.imageUrl = imageUrl;
