3e49fed304938bb51ca9b5d2c3ca83423ce7197f
Doris Rush-Lopez <rus19023@byui.edu>
2022-09-25 16:04:47 -0400

    fixed some style issues

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,17 +1,3 @@
 export class Contact {
-    public id: number;
-    public cname: string;
-    public email: string;
-    public phone: string;
-    public imageUrl: string;
-    public group: number[];
-
-    constructor(id: number, cname: string, email: string, phone: string, imageUrl: string, group: number[]) {
-        this.id = id;
-        this.cname = cname;
-        this.email = email;
-        this.phone = phone;
-        this.imageUrl = imageUrl;
-        this.group = group;
-    }
+    constructor(public id: number, public cname: string, public email: string, public phone: string, public imageUrl: string, public group: number[]) {}
 }
