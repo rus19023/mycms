b2e7cae075cbeb84500af7b3fb6adbfef2a7a3ca
rus19023 <rus19023@byui.edu>
2022-09-27 00:29:00 -0400

    updates to tests.py

--- python/tests.py
+++ python/tests.py
@@ -1,55 +1,57 @@
-
 def calculate_y_conditional(L, s, a, e, b, c):
-    #print(type(s))
-    #print('s: {}'.format(s))
-    xvals = [0, L * 2, L * -2]
-    yvals = []
-    #x = 0
-    # for i in range(L, 2):
-    #     xvals.append(x + i)
-    #     i += 1
-    print("")
-    print('xvals: {}'.format(xvals))
-    print("")
-    for x in xvals:
-        y = (a * (x ** e)) + (b * x) + c
-        #print('({},{})'.format(x, y))
-        #print("")
+    
+    y = (a * (0 ** e)) + (b * 0) + c
+    print('({},{})'.format(0, y))
+    y = (a * (L ** e)) + (b * L) + c
+    print('({},{})'.format(L, y))
 
-        match s:
-            case '<':
-                if x < L:
-                    #x += 1.01 if x < 0 else x
-                    #print('if x < L:')
-                    print('({},{})'.format(x, y))
-                    yvals.append(y)
-            case '<=':
-                if x <= L:
-                    #print('if x <= L:')
-                    print('({},{})'.format(x, y))
-                    yvals.append(y)
-            case '>':
-                if x > L:
-                    #print('if x > L:')
-                    print('({},{})'.format(x, y))
-                    yvals.append(y)
-            case '>=':
-                if x >= L:
-                    #print('if x >= L:')
-                    print('({},{})'.format(x, y))
-                    yvals.append(y)
-    #print('xvals: {}'.format(xvals))
-    #print('yvals: {}'.format(yvals))
+    # xvals = [0, L]
+    # yvals = []
+    # #x = 0
+    # # for i in range(L, 2):
+    # #     xvals.append(x + i)
+    # #     i += 1
+    # print("")
+    # print('xvals: {}'.format(xvals))
+    # print("")
+    # for x in xvals:
+    #     y = (a * (x ** e)) + (b * x) + c
+    #     #print('({},{})'.format(x, y))
+    #     #print("")
 
-L = -2
-s = '<='
-b = 1
-c = -3
+    #     match s:
+    #         case '<':
+    #             if x < L:
+    #                 #x += 1.01 if x < 0 else x
+    #                 #print('if x < L:')
+    #                 print('({},{})'.format(x, y))
+    #                 yvals.append(y)
+    #         case '<=':
+    #             if x <= L:
+    #                 #print('if x <= L:')
+    #                 print('({},{})'.format(x, y))
+    #                 yvals.append(y)
+    #         case '>':
+    #             if x > L:
+    #                 #print('if x > L:')
+    #                 print('({},{})'.format(x, y))
+    #                 yvals.append(y)
+    #         case '>=':
+    #             #if x >= L:
+    #                 #print('if x >= L:')
+    #                 print('({},{})'.format(x, y))
+    #                 yvals.append(y)
+    # #print('xvals: {}'.format(xvals))
+    # print('yvals: {}'.format(yvals))
 
+L = -1
+s = '<'
+b = -1
+c = 3
 
-s2 = '>'
-b2 = 4
-c2 = 5
+s2 = '>='
+b2 = -3
+c2 = 1
 
 
 
@@ -65,7 +67,7 @@ print('')
 print("first piece")
 print('____________________')
 
-calculate_y_conditional(L, s,  a, e, b, c)
+calculate_y_conditional(L, s, a, e, b, c)
 
 print('')
 print('between piece')
