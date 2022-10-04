6ef7a06f60600dd0ec03ffda487ae9eebd98435f
Doris Rush-Lopez <rus19023@byui.edu>
2022-10-03 20:30:00 -0400

    finish databinding nav

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'WeLearn CMS';
   username = '';
-  loadedFeature = 'contact';
+  selectedFeature = 'documents'
 
   switchView(selectedFeature: string) {
-    this.loadedFeature = selectedFeature;
+    this.selectedFeature = selectedFeature;
   }
 }
