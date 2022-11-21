b5ef28fedd7989efa4e7d23cc4a7e9d166f0f9f4
rus19023 <rus19023@byui.edu>
2022-11-21 07:16:56 -0500

    trying to fix documents no display

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Contact } from '../contact.model';
 import { ContactService } from '../contact.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cms-contact-list',
@@ -12,10 +13,17 @@ import { ContactService } from '../contact.service';
 export class ContactListComponent implements OnInit {
   contacts: Contact[];
   //@Input() index: number;
-
+  subscription: Subscription; 
   constructor(private contactService: ContactService) {}
 
   ngOnInit() {
+    this.subscription = this.contactService.contactChangedEvent  
+      .subscribe(
+      (contactsList: Contact[]) => {
+          this.contacts = contactsList;
+      }
+    );
+    console.log(`this.contacts: ${this.contacts}`);
     // Load the stored contacts
     this.contacts = this.contactService.getContacts();
   }   
