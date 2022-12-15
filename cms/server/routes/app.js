e3db08a9020a9174710cc0c039f88a974806a993
Doris Rush-Lopez <rus19023@byui.edu>
2022-12-15 12:36:30 -0500

    node server loads, but has blank body

--- cms/server/routes/app.js
+++ cms/server/routes/app.js
@@ -1,14 +1,11 @@
 var express = require('express');
-var router = express.Router();
 var path = require('path');
+var router = express.Router();
 
-// /* GET home page. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(process.cwd(), './src/index.html'), {title: 'CMS'});
-
-    //res.sendFile(path.join(__dirname, 'cms/index.html'), { title: 'CMS'});
-    //res.sendFile(, '/index.html', { title: 'CMS'}));
-    //res.sendFile('../../../src/index.html', { title: 'CMS'});
+    //res.sendFile(path.join('/index.html'), {title: 'WeLearn CMS'})
+    res.sendFile(path.join(process.cwd(), 'docs/index.html'), {title: '*** WeLearn CMS ***'})
 });
 
 module.exports = router;
